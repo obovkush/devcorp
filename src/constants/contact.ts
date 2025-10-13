646b87a7265ce885d643adca90fa53f4e0cbfdf5
Олег Бовкуш <o.bovkush@bftcom.com>
2025-10-13 23:06:48 +1000

    email: "support@solarm-tech.ru"

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,6 +1,6 @@
 export const CONTACT_INFO = {
   phone: "+7 (902) 970-68-23",
-  email: "info@solarmtech.ru",
+  email: "support@solarm-tech.ru",
   location: "Наши представители есть в Испании (Валенсия) и России (Москва, Киров, Красноярск, Хабаровск)",
   messengers: {
     whatsapp: "https://wa.me/79029706823",
