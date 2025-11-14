e19719a5eddaf7942210b41ca92f1cbb8b038f02
Олег Бовкуш <o.bovkush@bftcom.com>
2025-11-14 19:33:23 +1000

    Добавлен Санкт-Петербург в CONTACT_INFO

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,7 +1,7 @@
 export const CONTACT_INFO = {
   phone: "+7 (902) 970-68-23",
   email: "support@solarm-tech.ru",
-  location: "Наши представители есть в Испании (Валенсия) и России (Москва, Киров, Красноярск, Хабаровск)",
+  location: "Наши представители есть в Испании (Валенсия) и России (Санкт-Петербург, Москва, Киров, Красноярск, Хабаровск)",
   messengers: {
     whatsapp: "https://wa.me/79029706823",
     telegram: "https://t.me/aminlait"
