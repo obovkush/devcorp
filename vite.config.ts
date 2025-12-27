b59627599001aa1a2120d4e88cfdbea1b432a8c1
Олег Бовкуш <o.bovkush@bftcom.com>
2025-12-27 18:48:23 +1000

    Установлен vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,38 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import {componentTagger} from 'lovable-tagger';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({mode}) => ({
+    server: {
+        host: '::',
+        port: 8080,
     },
-  },
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (!assetInfo.name) return `[name]-[hash][extname]`;
-          const info = assetInfo.name.split('.');
-          const extType = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            return `images/[name]-[hash][extname]`;
-          }
-          if (/woff2|woff|ttf|otf|eot/i.test(extType)) {
-            return `fonts/[name]-[hash][extname]`;
-          }
-          return `[name]-[hash][extname]`;
+    plugins: [react(), mode === 'development' && componentTagger(), svgr()].filter(Boolean),
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
         },
-      },
     },
-  },
+    build: {
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (!assetInfo.name) return `[name]-[hash][extname]`;
+                    const info = assetInfo.name.split('.');
+                    const extType = info[info.length - 1];
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        return `images/[name]-[hash][extname]`;
+                    }
+                    if (/woff2|woff|ttf|otf|eot/i.test(extType)) {
+                        return `fonts/[name]-[hash][extname]`;
+                    }
+                    return `[name]-[hash][extname]`;
+                },
+            },
+        },
+    },
 }));
