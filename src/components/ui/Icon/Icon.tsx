1a7a365db4803c3665b6b7873856912cf9be833b
Олег Бовкуш <o.bovkush@bftcom.com>
2025-12-27 18:49:29 +1000

    Добавлен компонент Icon

---
+++ src/components/ui/Icon/Icon.tsx
@@ -0,0 +1,37 @@
+import React, {memo} from 'react';
+
+import {classNames} from '../../../lib/classNames';
+
+type SvgProps = Omit<React.SVGProps<SVGSVGElement>, 'onClick'>;
+
+interface IconBaseProps extends SvgProps {
+    className?: string;
+    Svg: React.VFC<React.SVGProps<SVGSVGElement>>;
+}
+
+interface NonClickableIconProps extends IconBaseProps {
+    clickable?: false;
+}
+
+interface ClickableBaseProps extends IconBaseProps {
+    clickable: true;
+    onClick: () => void;
+}
+
+type IconProps = NonClickableIconProps | ClickableBaseProps;
+
+export const Icon = memo((props: IconProps) => {
+    const {className = '', Svg, width = 32, height = 32, clickable, ...otherProps} = props;
+
+    const icon = <Svg className={classNames('', {}, [className])} width={width} height={height} {...otherProps} onClick={undefined} />;
+
+    if (clickable) {
+        return (
+            <button type='button' className={''} onClick={props.onClick} style={{height, width}}>
+                {icon}
+            </button>
+        );
+    }
+
+    return icon;
+});
