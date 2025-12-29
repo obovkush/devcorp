4712bbb2933eb449a9d8c55824715e5f41b188f1
Олег Бовкуш <o.bovkush@bftcom.com>
2025-12-29 21:32:04 +1000

    Доработаны облака

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -66,7 +66,7 @@ const Header = () => {
             : 'text-muted-foreground hover:text-foreground transition-colors';
 
     return (
-        <header className='sm:fixed top-0 w-full z-50 backdrop-blur-md bg-background/95'>
+        <header className='fixed top-0 z-50 w-full backdrop-blur-md bg-background/95'>
             <div className='container mx-auto px-4 lg:px-8 py-4'>
                 {/* Logo */}
                 {/* <div className="flex items-center space-x-2 lg:space-x-3 justify-start">
